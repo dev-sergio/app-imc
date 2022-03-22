167261b7dcfd332c69a489ee67051e7b1318e35b
Sergio Junior <sergio.junior@beltsysplus.com.br>
2022-03-22 17:56:22 -0300

    Layout finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MaterialApp(
     home: Home(),
   ));
@@ -21,6 +21,58 @@ class _HomeState extends State<Home> {
         title: const Text("Calculadora de IMC"),
         centerTitle: true,
         backgroundColor: Colors.amberAccent,
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))
+        ],
+      ),
+      backgroundColor: Colors.deepPurple,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Icon(Icons.person_outline,
+                size: 120.0, color: Colors.amberAccent),
+            const TextField(
+              style: TextStyle(color: Colors.white, fontSize: 25.0),
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Peso (Kg)",
+                labelStyle: TextStyle(
+                  color: Colors.amberAccent,
+                ),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const TextField(
+              style: TextStyle(color: Colors.white, fontSize: 25.0),
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(
+                  color: Colors.amberAccent,
+                ),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0, top: 10.0),
+              child: SizedBox(
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: const Color(0xffffd740)),
+                  onPressed: () {},
+                  child:
+                      const Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                ),
+              ),
+            ),
+            const Text("Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 30.0),
+            )
+          ],
+        ),
       ),
     );
   }
